a99308083a632d2fa1b9fad6862834abd71da86a
Obi-SSS <ayubicceanputra01@gmail.com>
2025-11-10 14:21:43 +0700

    Membuat menu dashboard alami

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -20,64 +20,137 @@ class _HomePageState extends State<HomePage> {
   final CarouselController _controllera = CarouselController();
   int _currentIndex = 0;
 
+  final icDOa = 'assets/images/ic_menu_doa.png';
+
   final posterList = const <String>[
-    'assets/images/ramadan-kareem.png',
     'assets/images/idl-adh.png',
-    'assets/images/idh-fitr.png',
+    'assets/images/idl-fotr.png',
+    'assets/images/ramadan-kareem.jpg',
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          children: <Widget>[
-            /**
-             * ===================
-             * MENU SECTION
-             * ===================
-             */
-            _buildMenuGridSection(),
+        child: widget(
+          child: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                // ===========================
+                // menu waktu sholat by Lokasi
+                // ===========================
+                _buildHeroSection(),
 
-            // =======================
-            //  menu grid section
-            // ========================
+                /**
+                 * ===================
+                 * MENU SECTION
+                 * ===================
+                 */
+                _buildMenuGridSection(),
 
-            // =======================
-            // menu item wiget
-            // =======================
-            _buildCarouselSection(),
-          ],
+                // =======================
+                //  menu hero wiget
+                // ========================
+
+                // =======================
+                // menu item wiget
+                // =======================
+                _buildCarouselSection(),
+              ],
+            ),
+          ),
         ),
       ),
     );
   }
 
-  Widget _buildMenuItem(String iconPath, String title, String routname) {
-    return InkWell(
-      onTap: () {},
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.05),
-              blurRadius: 4,
-              offset: const Offset(0, 2),
-            )
-            ],
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(iconPath, width: 35),
-            const SizedBox(height: 6,),
-            Text(
-              'title',
-              style: TextStyle(fontFamily: 'PoppinsRegular', fontSize: 13),
+  //================
+  //menu hero wiget
+  //================
+  Widget _buildHeroSection() {
+    return Stack(
+      clipBehavior: Clip.none,
+      children: [
+        Container(
+          width: double.infinity,
+          height: 290,
+          decoration: BoxDecoration(
+            color: Color(0xFFB3E5FC),
+            borderRadius: BorderRadius.only(
+              bottomRight: Radius.circular(30),
+              bottomLeft: Radius.circular(30),
             ),
-          ],
+            image: DecorationImage(image: AssetImage('assets/images/bg-afternoon.png'),        
+            fit: BoxFit.cover
+            ),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'Assalamu\'alaikum',
+                  style: TextStyle(
+                    fontFamily: 'PoppinsRegular',
+                    fontSize: 16,
+                    color: Colors.white70,
+                  ),
+                ),
+                Text(
+                  'Ngargoyoso',
+                  style: TextStyle(fontFamily: 'PoppinsSemiBold', fontSize: 22,color: Colors.white),
+                ),
+                Text(
+                  DateFormat('HH:mm').format(DateTime.now()),
+                  style: TextStyle(
+                    fontFamily: 'PoppinsBold',
+                    fontSize: 50,
+                    height: 1.2,
+                    color: Colors.white
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildMenuItem(String iconPath, String title, String routeName) {
+    return Material(
+      color: Colors.transparent,
+      child: InkWell(
+        onTap: () {
+          Navigator.pushNamed(context, routeName);
+        },
+        borderRadius: BorderRadius.circular(12),
+        splashColor: Colors.amber.withOpacity(0.2),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.05),
+                blurRadius: 4,
+                offset: const Offset(0, 2),
+              ),
+            ],
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(iconPath, width: 35),
+              const SizedBox(height: 6),
+              Text(
+                'title',
+                style: TextStyle(fontFamily: 'PoppinsRegular', fontSize: 13),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -89,6 +162,8 @@ class _HomePageState extends State<HomePage> {
       child: GridView.count(
         crossAxisCount: 4,
         shrinkWrap: true,
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 16,
         physics: NeverScrollableScrollPhysics(),
         children: [
           _buildMenuItem(
@@ -107,6 +182,11 @@ class _HomePageState extends State<HomePage> {
             '/doa',
           ),
           _buildMenuItem('assets/images/ic_menu_zakat.png', 'Zakat', '/doa'),
+          _buildMenuItem(
+            'assets/images/ic_menu_doa.png', //title
+            'doa harian', //item path
+            '/doa', //route name
+          ),
         ],
       ),
     );
